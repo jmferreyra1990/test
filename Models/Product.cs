2eca0150d69a57d82feb1e1e54a2d48169d7ac6a
jmferreyra1990 <124157722+jmferreyra1990@users.noreply.github.com>
2025-05-28 21:51:27 -0300

    Add files via upload

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,1 +1,9 @@
-
+namespace backend.Models
+{
+  public class Product
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+  }
+}
