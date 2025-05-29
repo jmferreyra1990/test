65bdcec92c26a70b9117dc6ce050b8bdd743ad83
jmferreyra1990 <124157722+jmferreyra1990@users.noreply.github.com>
2025-05-28 21:59:35 -0300

    Add files via upload

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+using backend.Models;
+
+namespace backend.Repositories
+{
+  public interface IProductRepository
+  {
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(int id);
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Product product);
+  }
+}
